624ccf438973dd7733454204f7087f1f16bcebc6
Engomo <laszlo.vadkert@gmail.com>
2021-09-06 12:08:43 +0200

    Initial commit

---
+++ src/main/java/training360/sv2021jvjbfkepesitovizsga/Training360Application.java
@@ -0,0 +1,13 @@
+package training360.sv2021jvjbfkepesitovizsga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Training360Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Training360Application.class, args);
+    }
+
+}
---
+++ src/test/java/training360/sv2021jvjbfkepesitovizsga/Training360ApplicationTests.java
@@ -0,0 +1,13 @@
+package training360.sv2021jvjbfkepesitovizsga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Training360ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
